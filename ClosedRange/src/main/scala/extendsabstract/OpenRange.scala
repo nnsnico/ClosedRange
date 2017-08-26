726a4fc5b095354da299d2a9116047b211eea152
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-08-27 01:14:10 +0900

    OpenRangeクラスがsuperクラスを呼んでいた問題を修正

--- ClosedRange/src/main/scala/extendsabstract/OpenRange.scala
+++ ClosedRange/src/main/scala/extendsabstract/OpenRange.scala
@@ -3,9 +3,9 @@ package extendsabstract
 class OpenRange(low: Int, high: Int) extends AbstRange {
   require(low < high)
 
-  override def getLowerEndPoint: Int = super.getLowerEndPoint
+  override def getLowerEndPoint: Int = low
 
-  override def getHigherEndPoint: Int = super.getHigherEndPoint
+  override def getHigherEndPoint: Int = high
 
   override def getIntersection(range: AbstRange): AbstRange =
     new ClosedRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
