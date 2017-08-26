ead5579c70474020fb3a92e5ebc5522defa4dd50
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-08-27 01:59:10 +0900

    OpenRangeオブジェクト作成，parseメソッド作成

--- ClosedRange/src/main/scala/extendsabstract/OpenRange.scala
+++ ClosedRange/src/main/scala/extendsabstract/OpenRange.scala
@@ -1,21 +1,23 @@
 package extendsabstract
 
-class OpenRange(low: Int, high: Int) extends AbstRange {
-  require(low < high)
-
-  override def getLowerEndPoint: Int = low
-
-  override def getHigherEndPoint: Int = high
-
-  override def getIntersection(range: AbstRange): AbstRange =
-    new ClosedRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
-
-  override def contains(base: Int): Boolean = if (low < base && high > base) true else false
-
-  override def parse(s: String): AbstRange = {
-    val parsedValue = s.trim.tail.init.split(",")
-    new OpenRange(parsedValue(0).toInt, parsedValue(1).toInt)
+object OpenRange {
+  def parse(s: String): OpenRange = {
+    val parsed = s.trim.tail.init.split(",")
+    new OpenRange(parsed(0).toInt, parsed(1).toInt)
   }
 
-  override def toString = s"($low,$high)"
+  class OpenRange(low: Int, high: Int) extends AbstRange {
+    require(low < high)
+
+    override def getLowerEndPoint: Int = low
+
+    override def getHigherEndPoint: Int = high
+
+    override def getIntersection(range: AbstRange): AbstRange =
+      new OpenRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
+
+    override def contains(base: Int): Boolean = if (low < base && high > base) true else false
+
+    override def toString = s"($low,$high)"
+  }
 }
