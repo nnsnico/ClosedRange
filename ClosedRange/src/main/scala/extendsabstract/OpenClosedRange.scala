8b0ca307251ab2dc5f976830892139559bdf153c
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-08-27 02:05:33 +0900

    半開区間も同様にオブジェクト作成，parseメソッド作成

--- ClosedRange/src/main/scala/extendsabstract/OpenClosedRange.scala
+++ ClosedRange/src/main/scala/extendsabstract/OpenClosedRange.scala
@@ -1,43 +1,47 @@
 package extendsabstract
 
 object OpenClosedRange {
-  class LClosedROpenRange(low: Int, high: Int) extends AbstRange {
-    require(low < high)
-
-    override def getLowerEndPoint: Int = low
-
-    override def getHigherEndPoint: Int = high
-
-    override def getIntersection(range: AbstRange): AbstRange =
-      new LClosedROpenRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
-
-    override def contains(base: Int): Boolean = if (low <= base && high > base) true else false
-
-    override def parse(s: String): AbstRange = {
-      val parsedValue = s.trim.tail.init.split(",")
-      new LClosedROpenRange(parsedValue(0).toInt, parsedValue(1).toInt)
+  object LClosedROpenRange {
+    def parse(s: String): LClosedROpenRange = {
+      val parsed = s.trim.tail.init.split(",")
+      new LClosedROpenRange(parsed(0).toInt, parsed(1).toInt)
     }
 
-    override def toString: String = s"[$low,$high)"
+    class LClosedROpenRange(low: Int, high: Int) extends AbstRange {
+      require(low < high)
+
+      override def getLowerEndPoint: Int = low
+
+      override def getHigherEndPoint: Int = high
+
+      override def getIntersection(range: AbstRange): AbstRange =
+        new LClosedROpenRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
+
+      override def contains(base: Int): Boolean = if (low <= base && high > base) true else false
+
+      override def toString: String = s"[$low,$high)"
+    }
   }
 
-  class LOpenRClosedRange(low: Int, high: Int) extends AbstRange {
-    require(low < high)
-
-    override def getLowerEndPoint: Int = low
-
-    override def getHigherEndPoint: Int = high
-
-    override def getIntersection(range: AbstRange): AbstRange =
-      new LClosedROpenRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
-
-    override def contains(base: Int): Boolean = if (low < base && high >= base) true else false
-
-    override def parse(s: String): AbstRange = {
-      val parsedValue = s.trim.tail.init.split(",")
-      new LOpenRClosedRange(parsedValue(0).toInt, parsedValue(1).toInt)
+  object LOpenRClosedRange {
+    def parse(s: String): LOpenRClosedRange = {
+      val parsed = s.trim.tail.init.split(",")
+      new LOpenRClosedRange(parsed(0).toInt, parsed(1).toInt)
     }
 
-    override def toString: String = s"($low,$high]"
+    class LOpenRClosedRange(low: Int, high: Int) extends AbstRange {
+      require(low < high)
+
+      override def getLowerEndPoint: Int = low
+
+      override def getHigherEndPoint: Int = high
+
+      override def getIntersection(range: AbstRange): AbstRange =
+        new LOpenRClosedRange(low max range.getLowerEndPoint, high min range.getHigherEndPoint)
+
+      override def contains(base: Int): Boolean = if (low < base && high >= base) true else false
+
+      override def toString: String = s"($low,$high]"
+    }
   }
 }
