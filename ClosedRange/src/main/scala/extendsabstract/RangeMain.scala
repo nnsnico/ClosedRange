574689cf5472663dfc17e6630ef7076f1ae173f5
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-08-29 00:08:59 +0900

    Mainオブジェクトに適用 オブジェクト名とクラス名がぶつかっているため，クラス名の頭にClassを置くよう変更

--- ClosedRange/src/main/scala/extendsabstract/RangeMain.scala
+++ ClosedRange/src/main/scala/extendsabstract/RangeMain.scala
@@ -1,33 +1,33 @@
 package extendsabstract
 
-import extendsabstract.ClosedRange.ClosedRange
-import extendsabstract.OpenClosedRange.LClosedROpenRange.LClosedROpenRange
-import extendsabstract.OpenClosedRange.LOpenRClosedRange.LOpenRClosedRange
+import extendsabstract.ClosedRange.{ClosedRange => ClassClosed}
+import extendsabstract.OpenRange.{OpenRange => ClassOpen}
 import extendsabstract.OpenClosedRange.LOpenRClosedRange
-import extendsabstract.OpenRange.OpenRange
+import extendsabstract.OpenClosedRange.LClosedROpenRange.{LClosedROpenRange => ClassLcRo}
+import extendsabstract.OpenClosedRange.LOpenRClosedRange.{LOpenRClosedRange => ClassLoRc}
 
 object RangeMain extends App {
-  val cRange: ClosedRange = new ClosedRange(3, 8)
+  val cRange: AbstRange = RangeFactory.newInstance(ClassClosed(3, 8))
   println(
     s"""ClosedRange: ${cRange.toString}
        |           : low = ${cRange.getLowerEndPoint}
        |           : high = ${cRange.getHigherEndPoint}
        |           : contains(4) = ${cRange.contains(4)}
      """.stripMargin)
-  val oRange: OpenRange = new OpenRange(3, 8)
+  val oRange: AbstRange = RangeFactory.newInstance(ClassOpen(3, 8))
   println(
     s"""OpenRange: ${oRange.toString}
        |         : low = ${oRange.getLowerEndPoint}
        |         : high = ${oRange.getHigherEndPoint}
        |         : contains(3) = ${oRange.contains(3)}
      """.stripMargin)
-  val lClosedROpen: LClosedROpenRange = new LClosedROpenRange(3, 8)
+  val lClosedROpen: AbstRange = RangeFactory.newInstance(ClassLcRo(3, 8))
   println(
     s"""Left Closed Right Open Range: ${lClosedROpen.toString}
        |                            : low = ${lClosedROpen.getLowerEndPoint}
        |                            : high = ${lClosedROpen.getHigherEndPoint}
      """.stripMargin)
-  var lOpenRClose: LOpenRClosedRange = new LOpenRClosedRange(3, 8)
+  var lOpenRClose: AbstRange = RangeFactory.newInstance(ClassLoRc(3, 8))
   println(
     s"""Left Open Right Closed Range: ${lOpenRClose.toString}
        |                            : parsed("(4,10]")  = ${lOpenRClose = LOpenRClosedRange.parse("(4,10]"); lOpenRClose.toString}
